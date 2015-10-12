64b688de4c597808ee82a8222e7c3fd7ce4c295b
Leif Gensert <leif@propertybase.com>
2015-10-12 14:32:49 -0700

    remove prod.secrets
    not needed in a simple echo app

--- config/prod.exs
+++ config/prod.exs
@@ -54,7 +54,3 @@ config :logger, level: :info
 #
 #     config :byzal, Byzal.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
